b492ee4d22f8aa42ad19b3bd4dbad9ff73c6c910
Prince Kumar <prkumar@leaptodigital.com>
2025-06-26 10:59:10 +0530

    Add Basic routing and footer component

---
+++ src/layout/Body.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+
+const Body = () => {
+    return (
+        <>
+            <NavBar />
+            <Outlet />
+            <Footer />
+        </>
+    );
+};
+
+export default Body;
