a6631f504c31f12c12a1a010bf4a59cb3c8a373a
Prince Kumar <prkumar@leaptodigital.com>
2025-06-26 17:51:32 +0530

    Implement logout functionality in NavBar and refactor links to use React Router

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,43 @@
+import axios from "axios";
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+import { removeUser } from "../utils/userSlice";
 
 const NavBar = () => {
-
     const user = useSelector((store) => store.user);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            await axios.post(
+                BASE_URL + "/auth/logout",
+                {},
+                { withCredentials: true }
+            );
+            dispatch(removeUser());
+            navigate("/login");
+        } catch (error) {
+            console.error("Logout failed:", error);
+        }
+    };
 
     return (
         <div>
             <div className="navbar bg-base-300 shadow-sm">
                 <div className="flex-1">
-                    <a className="btn btn-ghost text-xl">DevTinder</a>
+                    <Link to="/" className="btn btn-ghost text-xl">
+                        DevTinder
+                    </Link>
                 </div>
                 <div className="flex gap-2">
-                    {
-                        user && <div className="form-control flex items-center">Welcome {user && user?.firstName}</div>
-                    }
+                    {user && (
+                        <div className="form-control flex items-center">
+                            Welcome {user && user?.firstName}
+                        </div>
+                    )}
                     <div className="dropdown dropdown-end mx-5">
                         <div
                             tabIndex={0}
@@ -24,7 +47,11 @@ const NavBar = () => {
                             <div className="w-10 rounded-full">
                                 <img
                                     alt="Tailwind CSS Navbar component"
-                                    src={user && user?.photoUrl ? user?.photoUrl : "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"}
+                                    src={
+                                        user && user?.photoUrl
+                                            ? user?.photoUrl
+                                            : "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                                    }
                                 />
                             </div>
                         </div>
@@ -33,16 +60,16 @@ const NavBar = () => {
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
                         >
                             <li>
-                                <a className="justify-between">
+                                <Link to="/profile" className="justify-between">
                                     Profile
                                     <span className="badge">New</span>
-                                </a>
+                                </Link>
                             </li>
                             <li>
                                 <a>Settings</a>
                             </li>
                             <li>
-                                <a>Logout</a>
+                                <a onClick={handleLogout}>Logout</a>
                             </li>
                         </ul>
                     </div>
